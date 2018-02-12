1d1eadba7ac4fa5060668bbd06c0e2371c992efe
larry chang <lacha@microsoft.com>
2018-02-12 09:06:35 +0000

    fsfs

--- options/chart.js
+++ options/chart.js
@@ -672,4 +672,57 @@ function drawAllCharts() {
      }
    ]
    });
+
+// Build the chart
+Highcharts.chart('sku', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Total SKU Types Overview'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+	        enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'SKU-Type-1',
+            y: 56.33
+        }, {
+            name: 'SKU-Type-2',
+            y: 24.03
+        }, {
+            name: 'SKU-Type-3',
+            y: 10.38
+        }, {
+            name: 'SKU-Type-4',
+            y: 4.77
+        }, {
+            name: 'SKU-Type-5',
+            y: 0.91
+        }, {
+            name: 'SKU-Type-6',
+            y: 0.2
+        }]
+    }]
+});
 }
